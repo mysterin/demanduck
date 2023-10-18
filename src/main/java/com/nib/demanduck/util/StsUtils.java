0f5cdb375eb7343afae96aee4b21fe75bd4d8668
mysterin <mysterin@163.com>
2023-10-18 20:04:00 +0800

    阿里云工具类

---
+++ src/main/java/com/nib/demanduck/util/StsUtils.java
@@ -0,0 +1,20 @@
+package com.nib.demanduck.util;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author linxiaobin
+ * @Description 阿里云 sts 工具类
+ * @date 2023/10/18 20:03
+ */
+@Component
+public class StsUtils {
+
+    /**
+     * 获取 sts token
+     * @return
+     */
+    public String getStsToken() {
+        return null;
+    }
+}
